4ecda584419327c31c4d523af09e11ccc58db912
datshroud <letrandat8905@gmail.com>
2025-03-29 21:14:50 +0700

    Update product item

--- src/main/java/com/example/javafxapp/Main.java
+++ src/main/java/com/example/javafxapp/Main.java
@@ -23,51 +23,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         // quan li san pham 2
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/javafxapp/view/product/product.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/javafxapp/view/product/products.fxml"));
        Scene scene = new Scene(fxmlLoader.load());
        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/javafxapp/view/images/icons.jpg")));
        primaryStage.setScene(scene);
        primaryStage.setTitle("Coffee Shop Management");
        primaryStage.show();
-        
-        //  login2
-        //  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/javafxapp/view/login_signup/signin.fxml"));
-        //  Scene scene = new Scene(fxmlLoader.load());
-        //  primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/javafxapp/view/images/icons.jpg")));
-        //  primaryStage.setScene(scene);
-        //  primaryStage.setTitle("Coffee Shop Management");
-        //  primaryStage.show();
-
-        //  login
-        //  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/javafxapp/view/login_signup/login_signup.fxml"));
-        //  Scene scene = new Scene(fxmlLoader.load());
-        //  primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/javafxapp/view/images/icons.jpg")));
-        //  primaryStage.setScene(scene);
-        //  primaryStage.setTitle("Coffee Shop Management");
-        //  primaryStage.setResizable(false);
-        //  primaryStage.show();
-
-        // quan li san pham
-        //    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/javafxapp/view/product/product.fxml"));
-        //    Scene scene = new Scene(fxmlLoader.load());
-        //    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/javafxapp/view/images/icons.jpg")));
-        //    primaryStage.setScene(scene);
-        //    primaryStage.setTitle("Coffee Shop Management");
-        //    primaryStage.show();
-
-        // add quyen
-        // FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/javafxapp/view/Role/RoleForm.fxml"));
-        // Scene scene = new Scene(fxmlLoader.load(), 400, 300);
-        // primaryStage.setTitle("Thêm Quyền");
-        // primaryStage.setScene(scene);
-        // primaryStage.show();
-
-        // add account
-        // FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/javafxapp/view/account/addAccount.fxml"));
-        // Scene scene = new Scene(fxmlLoader.load(), 400, 300);
-        // primaryStage.setTitle("Thêm Quyền");
-        // primaryStage.setScene(scene);
-        // primaryStage.show();
     }
 
     public static void main(String[] args) {
