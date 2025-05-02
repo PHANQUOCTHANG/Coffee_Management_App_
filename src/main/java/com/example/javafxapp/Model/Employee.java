2c307600a07065ac9be1bd5b9ce624527d6ff3be
TramThuy-11A <nguyenthuytram102005@gmail.com>
2025-05-02 15:58:10 +0700

    EmployeeReposiytory và EmployeeModel

---
+++ src/main/java/com/example/javafxapp/Model/Employee.java
@@ -0,0 +1,53 @@
+package com.example.javafxapp.Model;
+
+public class Employee {
+    private int employee_id ;
+    private String fullName ;
+    private String phone ;
+    private boolean deleted = false  ;
+
+    public Employee(String fullName, String phone) {
+        this.fullName = fullName;
+        this.phone = phone;
+    }
+
+    public Employee(int employee_id, String fullName, String phone, boolean deleted) {
+        this.employee_id = employee_id;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.deleted = deleted;
+    }
+
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
+
